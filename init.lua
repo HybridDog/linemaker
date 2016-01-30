9eb8021c46aeb214ba98ec1a77e3acfbb8d983d4
HybridDog <ovvv@web.de>
2016-01-30 14:25:39 +0100

    :boom:

---
+++ init.lua
@@ -0,0 +1,117 @@
+local load_time_start = os.clock()
+
+
+local creative_enabled = minetest.setting_getbool("creative_mode")
+
+local playerdata = {}
+local tool_active
+
+minetest.register_tool("linemaker:tool", {
+	description = "pull lines",
+	inventory_image = "linemaker.png",
+	stack_max = 1,
+	--node_placement_prediction = nil,
+	on_place = function(itemstack, player, pt)
+		if not player
+		or not pt then
+			return
+		end
+
+		tool_active = true
+
+		local pname = player:get_player_name()
+
+		playerdata[pname] = {
+			range = 6,--vector.subtract()
+			ps = {[0] = pt.under, pt.above},
+		}
+--[[
+		local keys = placer:get_player_control()
+		local name = placer:get_player_name()
+
+		if keys.aux1 then
+			local item = itemstack:to_table()
+			local node, mode = get_data(item)
+			mode = modes[modes[mode]%#modes+1]
+			set_data(item, node, mode)
+			itemstack:replace(item)
+			inform(name, "Mode changed to: "..mode..": "..mode_infos[mode])
+			return itemstack
+		end
+
+		-- just place the stored node if now new one is to be selected
+		if not keys.sneak then
+			return replacer.replace(itemstack, placer, pt, true)
+		end
+
+
+		if pt.type ~= "node" then
+			inform(name, "	Error: No node selected.")
+			return
+		end
+
+		local item = itemstack:to_table()
+		local node, mode = get_data(item)
+
+		node = minetest.get_node_or_nil(pt.under) or node
+
+		set_data(item, node, mode)
+		itemstack:replace(item)]]
+	end,
+})
+
+-- update to new positions
+minetest.register_globalstep(function(dtime)
+	-- abort if noone uses it
+	if not tool_active then
+		return
+	end
+
+	--[[ abort that it doesn't shoot too often (change it if your pc runs faster)
+	timer = timer+dtime
+	if timer < 0.1 then
+		return
+	end
+	timer = 0 --]]
+
+	local active
+	for pname,data in pairs(playerdata) do
+		local player = minetest.get_player_by_name(pname)
+
+		local ps = data.ps
+		local wantedpos = vector.round(
+			vector.add(
+				player:getpos(),
+				vector.multiply(player:get_look_dir(), data.range)
+			)
+		)
+		if not vector.equals(ps[#ps], wantedpos) then
+			for i,p in pairs(vector.line(ps[1], wantedpos) do
+				ps[i] = p
+			end
+		end
+
+		if player:get_wielded_item():to_string() == "linemaker:tool"
+		and player:get_player_control().RMB then
+			active = true
+		else
+			for i = 1,#ps do
+				minetest.set_node(ps[i], {name="default:wood"})
+			end
+			playerdata[pname] = nil
+		end
+	end
+
+	-- disable the function if noone currently uses it to reduce lag
+	if not active then
+		tool_active = false
+	end
+end)
+
+local time = math.floor(tonumber(os.clock()-load_time_start)*100+0.5)/100
+local msg = "[linemaker] loaded after ca. "..time
+if time > 0.05 then
+	print(msg)
+else
+	minetest.log("info", msg)
+end
